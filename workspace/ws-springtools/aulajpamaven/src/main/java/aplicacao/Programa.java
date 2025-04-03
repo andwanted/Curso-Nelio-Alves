3408dc6e7b549048127140dd8a139aeab24bfd39
Anderson leandro <andersonleandrooo@hotmail.com>
2025-04-03 11:31:28 -0300

    exemplo de busca

--- workspace/ws-springtools/aulajpamaven/src/main/java/aplicacao/Programa.java
+++ workspace/ws-springtools/aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -10,21 +10,22 @@ public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Pessoa P1 = new Pessoa(null,"Carlos da Silva","carlos@gmail.com");
-		Pessoa P2 = new Pessoa(null,"Juca Oliveira","juca@gmail.com");
-		Pessoa P3 = new Pessoa(null,"Roberto Souza","roberts@gmail.com");
+	
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
-		em.persist(P1);
-		em.persist(P2);
-		em.persist(P3);
-		em.getTransaction().commit();		
 		
-		System.out.println(P1);
-		System.out.println(P2);
-		System.out.println(P3);
+		Pessoa p = em.find(Pessoa.class, 2);
+		
+		System.out.println(p);
+		System.out.println("Pronto!!");
+		
+		em.close();
+		emf.close();
+		
+		
+		
 	}
 
 }
